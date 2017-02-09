ec8e661d1de59f0fb6b73cd64a2b1dd77bab6bf5
Cam <cbuscaron@gmail.com>
2017-02-08 20:04:34 -0800

    clean

--- morgans_book/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/cam/ROS/morgans_book/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/cam/ROS/morgans_book/catkin_ws/devel;/home/cam/catkin_ws/devel;/home/cam/indigo/devel;/opt/ros/indigo".split(';') if "/home/cam/ROS/morgans_book/catkin_ws/devel;/home/cam/catkin_ws/devel;/home/cam/indigo/devel;/opt/ros/indigo" != "" else []
--- morgans_book/catkin_ws/build/my_awesome_code/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cam/ROS/morgans_book/catkin_ws/src/my_awesome_code
-# Build directory: /home/cam/ROS/morgans_book/catkin_ws/build/my_awesome_code
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
